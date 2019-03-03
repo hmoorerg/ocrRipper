f87fbdec4b274c04d06867a0b412bdb1d124d58f
Henry <hmoorerg@gmail.com>
2019-03-03 09:53:14 -0800

    Fixed the code to use average position to find a text's area

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask,flash,jsonify,redirect,url_for, request, render_template,send_from_directory
 app = Flask(__name__)
 import base64
-
-app.secret_key = b'_5#yzxcxzcxz2L"F4Q8z\n\xec]/'
-
 from google.cloud import vision
+import json
 
 client = vision.ImageAnnotatorClient()
 
@@ -13,18 +11,6 @@ client = vision.ImageAnnotatorClient()
 def index():
     return send_from_directory("public","index.html")
 
-import json
-
-# @app.route("/<location>")
-# def hello(location):
-#     location = 'https://help.r2docuo.com/en/lib/R2Docuo_App_Sign_1.png'
-#     response = client.annotate_image({
-#         'image': {'source': {'image_uri': location}},
-#         'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}]
-#     })
-
-#     found_txt = response.text_annotations
-#     return json.dumps(found_txt[4].description)
 
 data = {}
 
@@ -34,7 +20,7 @@ def clear():
     # Reset the 'Database'
     data = {}
 
-    return "Done clearing data"
+    return redirect(url_for('lst'))
 
 @app.route("/list")
 def lst():
@@ -56,7 +42,6 @@ def upload():
         'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}]
     }
 
-    flash("Read image")
     response = client.annotate_image(config)
 
     found_txt = response.text_annotations
@@ -70,26 +55,24 @@ def upload():
     for txt in found_txt:
         vertices = txt.bounding_poly.vertices
         sort_vertices(vertices)
+        
         (start_x,start_y) = (vertices[0].x,vertices[0].y)
-        
-        
         (end_x,end_y) = (vertices[3].x,vertices[3].y)
-
         avg_x = (start_x + end_x) / 2
-        avg_y = (start_y + end_y] / 2
+        avg_y = (start_y + end_y) / 2
+
 
         # Name
-        if start_x > 560 and end_x < 1000:
-            if start_y > 700 and end_y < 919:
-                print(txt.description)
+        if 650 < avg_x < 1000:
+            if  700 < avg_y < 919:
                 name += " "+txt.description
+                # print(avg_x,avg_y,txt.description, sep="\t")
         
         # Date
-        if start_x > 370 and end_x < 600:
-            if start_y > 700 and end_y < 910:
-                if not (txt.description in ['Signature:','(print)',"Name","(orint","(orint)"]):
-                    print(txt.description)
-                    date += txt.description
+        if 400 < avg_x < 600:
+            if  830 < avg_y < 910:
+                date += txt.description
+                # print(avg_x,avg_y,txt.description, sep="\t")
         
 
     data[name] = date
@@ -98,3 +81,4 @@ def upload():
         
     return redirect(url_for('lst'))
 
+#app.run()
